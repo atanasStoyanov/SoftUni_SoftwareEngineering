a01241fbaf9cd9b17282745a25db16dd8b81264d
atanasStoyanov <atanass@dxc.com>
2019-09-30 22:55:27 +0300

    Data types and variables more exercise

---
+++ JS_Fundamentals/data types and variables_more exercise/chessBoard.js
@@ -0,0 +1,38 @@
+function solve(n) {
+    console.log('<div class="chessboard">');
+    
+    for (let i = 0; i < n; i++) {
+        console.log('  <div>');
+
+        if (i % 2 === 0) {
+
+            for (let j = 0; j < n; j++) {
+
+                if (j % 2 === 0) {
+                    console.log('    <span class="black"></span>');
+                } else {
+                    console.log('    <span class="white"></span>');
+                    
+                }            
+            }
+
+        } else {
+
+            for (let k = 0; k < n; k++) {
+
+                if (k % 2 === 0) {
+                    console.log('    <span class="white"></span>');
+                } else {
+                    console.log('    <span class="black"></span>');
+                    
+                }            
+            }
+        }
+
+        console.log('  </div>');
+    }
+
+    console.log('</div>');
+}
+
+solve(3);
---
+++ JS_Fundamentals/data types and variables_more exercise/triangleArea.js
@@ -0,0 +1,8 @@
+function solve(a, b, c) {
+    let semiPerimeter = (a + b + c) / 2;
+    let area = Math.sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+
+    console.log(area);
+}
+
+solve(2, 3.5, 4)

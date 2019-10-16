879fa2853df354a59b64efd55d9fde5797002ace
atanasStoyanov <atanass@dxc.com>
2019-10-16 23:13:42 +0300

    Advanced arrays

--- JS_Fundamentals/arrays_advanced/processOddNumbers.js
+++ JS_Fundamentals/arrays_advanced/processOddNumbers.js
@@ -1,3 +1,11 @@
-function solve(param) {
-    
+function solve(arr) {
+    let resultArr = arr
+        .filter((e, i) => i % 2 !== 0)
+        .map(a => a * 2)
+        .reverse()
+        .join(' ');
+
+    console.log(resultArr);
 }
+
+solve([10, 15, 20, 25]);

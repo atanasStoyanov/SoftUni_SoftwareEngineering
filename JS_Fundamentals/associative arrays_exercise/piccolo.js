354d5ebe4d3e92d71c1f0d7ca475ad23293a5f68
atanasStoyanov <atanass@dxc.com>
2019-11-08 16:38:18 +0200

    Associative arrays exercise

---
+++ JS_Fundamentals/associative arrays_exercise/piccolo.js
@@ -0,0 +1,33 @@
+function solve(arr) {
+
+    let parkingLot = arr
+        .reduce((agr, command) => {
+            let [action, car] = command.split(', ');
+            agr[car] = action;
+            return agr;
+        }, {})
+
+    let carsIn = Object.keys(parkingLot)
+        .filter(a => parkingLot[a] === 'IN');
+
+    if (carsIn.length > 0) {
+        carsIn
+            .sort()
+            .map(a => console.log(a));
+    } else {
+        console.log('Parking Lot is Empty');
+    }
+
+}
+
+solve([
+    'IN, CA2844AA',
+    'IN, CA1234TA',
+    'OUT, CA2844AA',
+    'IN, CA9999TT',
+    'IN, CA2866HI',
+    'OUT, CA1234TA',
+    'IN, CA2844AA',
+    'OUT, CA2866HI',
+    'IN, CA9876HH',
+    'IN, CA2822UU'])

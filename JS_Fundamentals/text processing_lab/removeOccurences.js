8469fd04565211f649a5a70b52598a280edfed1d
atanasStoyanov <atanass@dxc.com>
2019-11-14 19:23:39 +0200

    Text processing lab

---
+++ JS_Fundamentals/text processing_lab/removeOccurences.js
@@ -0,0 +1,13 @@
+function solve(word, text) {
+    
+    while(text.includes(word)) {
+        text = text.replace(word, '');
+    }
+
+    console.log(text);
+    
+}
+
+solve('ice',
+    'kicegiciceeb'
+);

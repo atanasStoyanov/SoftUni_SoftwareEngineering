d97f78ab390e95ff443914c4a2292f820471551b
atanasStoyanov <atanass@dxc.com>
2019-11-16 11:03:07 +0200

    Text processing more exercise

---
+++ JS_Fundamentals/text processing_exercise/hardWords.js
@@ -0,0 +1,14 @@
+function solve(arr) {
+    let letter = arr.shift();
+    let wordsArr = arr
+        .shift()
+        .sort((a, b) => b.length - a.length)
+        .map(word => letter = letter.replace('_'.repeat(word.length), word));
+
+    console.log(letter);
+}
+
+solve([
+    'Hi, grandma! I\'m so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother\'s ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.',
+    ['pie', 'bring', 'glad', 'During', 'amazing', 'pharmacist', 'sprained']
+]);

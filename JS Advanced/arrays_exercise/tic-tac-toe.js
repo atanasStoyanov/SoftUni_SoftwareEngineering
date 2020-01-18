c2f4a8875cfaa5e5a0a539161f9f5ccd6d6cd3c4
atanasStoyanov <atanass@dxc.com>
2020-01-18 19:22:39 +0200

    Arrays exercise

--- JS Advanced/arrays_exercise/tic-tac-toe.js
+++ JS Advanced/arrays_exercise/tic-tac-toe.js
@@ -5,73 +5,64 @@ function solve(arr) {
         [false, false, false]
     ];
 
+    const printDashboard = (arr) => arr
+        .map(row => row.join('\t'))
+        .join('\n');
+
+    const areEqualElements = (arr) => arr.every(el => el !== false && el === arr[0]);
+
+    const playerSwap = (player) => player === 'X' ? player = 'O' : player = 'X';
+
+    const boardIsNotFull = (arr) => arr.some(row => row.some(field => field === false));
+
     let output = '';
-    let playerIndex = 0;
-    let movesCounter = 0;
+    let player = 'X'
 
     for (let i = 0; i < arr.length; i++) {
         let [row, col] = arr[i].split(' ');
-        let player = '';
-
-        if (playerIndex % 2 === 0) {
-            player = 'X';
-        } else {
-            player = 'O'
-        }
 
         if (dashboard[row][col] === false) {
             dashboard[row][col] = player;
-            movesCounter++;
+            player = playerSwap(player);
         } else {
             output += 'This place is already taken. Please choose another!\n';
-            playerIndex--;
         }
 
-        playerIndex++;
-
-        let haveWinner = false;
+        let haveWinnerByRowOrCol = false;
         let firstDiagonalIsWinner = false;
         let secondDiagonalIsWinner = false;
+        let firstDiagonalArr = [];
+        let secondDiagonalArr = [];
 
-        if (movesCounter >= 5) {
-            let firstDiagonalArr = [];
-            let secondDiagonalArr = [];
+        for (let row = 0; row < dashboard.length; row++) {
+            let isRowWinner = areEqualElements(dashboard[row]);
+            let columnArr = dashboard
+                .reduce((agr, el) => {
+                    agr.push(el[row])
+                    return agr;
+                }, []);
 
-            for (let row = 0; row < dashboard.length; row++) {
-                let isRowWinner = dashboard[row].every(el => el !== false && el === dashboard[row][0]);
-                let isColumnWinner = dashboard
-                    .reduce((agr, el) => {
-                        agr.push(el[row])
-                        return agr;
-                    }, [])
-                    .every((el, i, arr) => el === arr[0]);
+            let isColumnWinner = areEqualElements(columnArr);
 
-                firstDiagonalArr.push(dashboard[row][row]);
-                secondDiagonalArr.push(dashboard[row][dashboard.length - 1 - row]);
+            firstDiagonalArr.push(dashboard[row][row]);
+            secondDiagonalArr.push(dashboard[row][dashboard.length - 1 - row]);
 
-                if (isRowWinner === true || isColumnWinner === true) {
-                    haveWinner = true;
-                    break;
-                }
+            if (isRowWinner === true || isColumnWinner === true) {
+                haveWinnerByRowOrCol = true;
+                break;
             }
-
-            firstDiagonalIsWinner = firstDiagonalArr.every(el => el === firstDiagonalArr[0]);
-            secondDiagonalIsWinner = secondDiagonalArr.every(el => el === secondDiagonalArr[0]);
         }
 
-        if (haveWinner || firstDiagonalIsWinner || secondDiagonalIsWinner) {
-            output += `Player ${player} wins!\n`;
-            output += dashboard
-                .map(row => row.join('\t'))
-                .join('\n');
+        firstDiagonalIsWinner = areEqualElements(firstDiagonalArr);
+        secondDiagonalIsWinner = areEqualElements(secondDiagonalArr);
 
+        if (haveWinnerByRowOrCol || firstDiagonalIsWinner || secondDiagonalIsWinner) {
+            output += `Player ${player = playerSwap(player)} wins!\n`;
+            output += printDashboard(dashboard);
             break;
-        } else if (movesCounter === 9) {
+        } else if (boardIsNotFull(dashboard) === false) {
             output += 'The game ended! Nobody wins :(\n';
-            output += dashboard
-                .map(row => row.join('\t'))
-                .join('\n');
-
+            output += printDashboard(dashboard);
             break;
         }
     }
@@ -80,14 +71,15 @@ function solve(arr) {
 }
 
 console.log(solve([
-    "0 1",
     "0 0",
-    "0 2",
-    "2 0",
-    "1 0",
-    "1 2",
+    "0 0",
     "1 1",
-    "2 1",
+    "0 1",
+    "1 2",
+    "0 2",
     "2 2",
-    "0 0"
+    "1 2",
+    "2 2",
+    "2 1"
+
 ]));

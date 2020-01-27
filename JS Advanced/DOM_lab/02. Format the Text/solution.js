d13ebc3b4394c86150d8a2fbce73d01e4145d6ae
atanasStoyanov <atanass@dxc.com>
2020-01-27 17:29:41 +0200

    DOM lab second problem done

--- JS Advanced/DOM_lab/02. Format the Text/solution.js
+++ JS Advanced/DOM_lab/02. Format the Text/solution.js
@@ -1,3 +1,40 @@
+// This will not work in Judge as I changed the HTML. 
+// To work in judge remove eventListner and add "onclick" to the button in the HTML.
+
 function solve() {
-  //TODO...
+  let input = document.getElementById('input');
+  let output = document.getElementById('output');
+
+  if (input === null || output === null) {
+    throw new Error('Error...');
+  }
+
+  const isNotEmptyString = str => str !== '';
+
+  let sentences = input.innerHTML
+    .split('.')
+    .filter(isNotEmptyString)
+    .map(e => e + '.');
+
+  for (let i = 0; i < sentences.length; i += 3) {
+    let p = document.createElement('p');
+    let str = '';
+
+    for (let j = 0; j < 3; j++) {
+
+      if (i + j < sentences.length) {
+        str += sentences[i + j];
+      }
+    }
+
+    p.innerHTML = str;
+    output.appendChild(p);
+  }
+
 }
+
+document.addEventListener('DOMContentLoaded', x => {
+  document
+    .getElementById('formatItBtn')
+    .addEventListener('click', solve);
+})

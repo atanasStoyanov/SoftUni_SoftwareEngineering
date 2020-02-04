63fdda0e998ec0cc8e73f81dce586ccc56ba177d
atanasStoyanov <atanass@dxc.com>
2020-02-04 23:01:09 +0200

    Classes exercise problem 3 done

---
+++ JS Advanced/Classes_exercise/unity.js
@@ -0,0 +1,40 @@
+class Rat {
+    constructor(name){
+        this.name = name;
+        this.unitedRats = [];
+    }
+
+    unite (otherRat){
+        
+        if (otherRat instanceof Rat) {
+            this.unitedRats.push(otherRat);
+        }
+    }
+
+    getRats(){
+        return this.unitedRats;
+    }
+
+    toString() {
+        let output = this.name;
+        this.unitedRats.map(rat => output += `\n##${rat}`);
+        return output;
+    }
+
+}
+
+
+let firstRat = new Rat("Peter");
+console.log(firstRat.toString()); // Peter
+console.log(firstRat.getRats()); // []
+
+firstRat.unite(new Rat("George"));
+firstRat.unite(new Rat("Alex"));
+console.log(firstRat.getRats());
+// // [ Rat { name: 'George', unitedRats: [] },
+// //  Rat { name: 'Alex', unitedRats: [] } ]
+
+console.log(firstRat.toString());
+// // Peter
+// // ##George
+// // ##Alex

325f742f2864059fd90f68932c7aaa19d88b52e0
atanasStoyanov <atanass@dxc.com>
2020-03-04 23:35:56 +0200

    Prototypes and inheritance lab problem 1 done:

---
+++ JS Applications/prototypes and inheritance_lab/1.Person and Teacher.js
@@ -0,0 +1,21 @@
+function sole() {
+
+    class Person {
+        constructor(n, m){
+            this.name = n;
+            this.email = m;
+        }
+    }
+
+    class Teacher extends Person {
+        constructor(n, m, s) {
+            super(n, m);
+            this.subject = s;
+        }
+    }
+
+    return {
+        Person,
+        Teacher
+    }
+}

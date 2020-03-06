9cdff859bc135fde1e66f204c262102b29b563c0
atanasStoyanov <atanass@dxc.com>
2020-03-07 00:13:17 +0200

    Prototypes and inheritance exercise problem 1 done

---
+++ JS Applications/prototypes and inheritance_exercise/1.Balloons.js
@@ -0,0 +1,48 @@
+function solve() {
+
+    class Balloon {
+        constructor(color, gasWeight) {
+            this.color = color;
+            this.gasWeight = gasWeight;
+        }
+    }
+
+    class PartyBalloon extends Balloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength) {
+            super(color, gasWeight);
+            this.ribbon = {
+                color: ribbonColor,
+                length: ribbonLength
+            }
+        }
+
+        get ribbon() {
+            return this._ribbon;
+        }
+
+        set ribbon(value) {
+            this._ribbon = value;
+        }
+    }
+
+    class BirthdayBalloon extends PartyBalloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength, text) {
+            super(color, gasWeight, ribbonColor, ribbonLength);
+            this.text = text;
+        }
+
+        get text() {
+            return this._text;
+        }
+
+        set text(value) {
+            this._text = value;
+        }
+    }
+
+    return {
+        Balloon,
+        PartyBalloon,
+        BirthdayBalloon
+    }
+}

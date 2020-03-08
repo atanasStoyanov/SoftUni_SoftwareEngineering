28badb99c4869f94ac713eb554137421dc353196
atanasStoyanov <atanass@dxc.com>
2020-03-08 12:49:27 +0200

    Prototypes and inheritance exercise problem 6 done

---
+++ JS Applications/prototypes and inheritance_exercise/6.Computer.js
@@ -0,0 +1,122 @@
+function result() {
+    class Base {
+        constructor(manufacturer) {
+            this.manufacturer = manufacturer;
+        }
+    }
+
+    class Keyboard extends Base {
+        constructor(manufacturer, responseTime) {
+            super(manufacturer);
+            this.responseTime = responseTime;
+        }
+    }
+
+    class Monitor extends Base {
+        constructor(manufacturer, width, height) {
+            super(manufacturer);
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    class Battery extends Base {
+        constructor(manufacturer, expectedLife) {
+            super(manufacturer);
+            this.expectedLife = expectedLife;
+        }
+    }
+
+    class Computer extends Base {
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace) {
+            if (new.target === Computer) {
+                throw new TypeError("Abstract class cannot be instantiated directly");
+            }
+
+            super(manufacturer);
+            this.processorSpeed = processorSpeed;
+            this.ram = ram;
+            this.hardDiskSpace = hardDiskSpace;
+        }
+    }
+
+    class Laptop extends Computer {
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace, weight, color, battery) {
+            super(manufacturer, processorSpeed, ram, hardDiskSpace);
+            this.weight = weight;
+            this.color = color;
+            this.battery = battery;
+        }
+
+        get battery() {
+            return this._battery;
+        }
+
+        set battery(obj) {
+            if (obj instanceof Battery) {
+                this._battery = obj
+            } else {
+                throw TypeError('Argument object is not instance of Battery class');
+            }
+        }
+    }
+
+    class Desktop extends Computer {
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace, keyboard, monitor) {
+            super(manufacturer, processorSpeed, ram, hardDiskSpace);
+            this.keyboard = keyboard;
+            this.monitor = monitor;
+        }
+
+        get keyboard() {
+            return this._keyboard;
+        }
+
+        set keyboard(obj) {
+            if (obj instanceof Keyboard) {
+                this._keyboard = obj;
+            } else {
+                throw TypeError('Argument object is not instance of Keyboard class');
+            }
+        }
+
+        get monitor() {
+            return this._monitor;
+        }
+
+        set monitor(obj) {
+            if (obj instanceof Monitor) {
+                this._monitor = obj;
+            } else {
+                throw TypeError('Argument object is not instance of Monitor class');
+            }
+        }
+    }
+
+    return {
+        Battery,
+        Keyboard,
+        Monitor,
+        Computer,
+        Laptop,
+        Desktop
+    }
+}
+
+
+let classes = result();
+let Computer = classes.Computer;
+let Laptop = classes.Laptop;
+let Desktop = classes.Desktop;
+let Monitor = classes.Monitor;
+let Battery = classes.Battery;
+let Keyboard = classes.Keyboard;
+
+let keyboard = new Keyboard('Logitech',70);
+let monitor = new Monitor('Benq',28,18);
+let battery = new Battery('Energy',3);
+let laptop = new Laptop("Hewlett Packard",2.4,4,0.5,3.12,"Silver",battery);
+let desktop = new Desktop("JAR Computers",3.3,8,1,keyboard,monitor);
+
+desktop.keyboard.manufacturer = 'gosho';
+console.log(desktop)

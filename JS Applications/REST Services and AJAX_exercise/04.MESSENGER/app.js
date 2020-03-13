05276ed36f541fd3284b676b14249783e6e6200b
atanasStoyanov <atanass@dxc.com>
2020-03-14 00:35:12 +0200

    REST Services and AJAX exercise problem 4 done

--- JS Applications/REST Services and AJAX_exercise/04.MESSENGER/app.js
+++ JS Applications/REST Services and AJAX_exercise/04.MESSENGER/app.js
@@ -1,5 +1,69 @@
 function attachEvents() {
-    console.log('TODO...');
+    const elements = {
+        authorInput: document.querySelector('#author'),
+        contentInput: document.querySelector('#content'),
+        sendBtn: document.querySelector('#submit'),
+        refreshBtn: document.querySelector('#refresh'),
+        textArea: document.querySelector('#messages')
+    }
+
+    const url = 'https://rest-messanger.firebaseio.com/messanger.json';
+
+    elements.sendBtn.addEventListener('click', addMessage);
+
+    elements.refreshBtn.addEventListener('click', loadMessages);
+
+
+    function addMessage(e) {
+        e.preventDefault();
+
+        const author = elements.authorInput.value;
+        const content = elements.contentInput.value;
+
+        const postRequestObj = {
+            method: 'POST',
+            body: JSON.stringify({
+                author,
+                content
+            })
+        }
+
+        fetch(url, postRequestObj)
+            .then()
+            .catch(errorHandler)
+
+        elements.authorInput.value = '';
+        elements.contentInput.value = '';
+    }
+
+    function loadMessages(e) {
+        e.preventDefault();
+
+        fetch(url)
+            .then(jsonParse)
+            .then(displayMessages)
+            .catch(errorHandler);
+    }
+
+    function displayMessages(data) {
+        let allMessages = formatMessagesToString(data);
+        elements.textArea.value = allMessages;
+    };
+
+    function formatMessagesToString(messageObj) {
+
+        return Object.values(messageObj)
+            .map(formatCurrentMessage)
+            .join('\n');
+    }
+
+    const jsonParse = (res) => res.json();
+
+    const formatCurrentMessage = ({ author, content }) => `${author}: ${content}`;
+
+    const errorHandler = (e) => {
+        console.dir(e);
+    };
 }
 
 attachEvents();

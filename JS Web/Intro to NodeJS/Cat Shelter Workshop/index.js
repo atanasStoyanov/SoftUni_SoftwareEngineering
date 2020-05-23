391a0a5be947884909c7e66ea134ce8ccf9901c8
atanasStoyanov <atanass@dxc.com>
2020-05-24 00:30:56 +0300

    home page

--- JS Web/Intro to NodeJS/Cat Shelter Workshop/index.js
+++ JS Web/Intro to NodeJS/Cat Shelter Workshop/index.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const port = 3000;
+const handlers = require('./handlers');
 
 http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-
-    res.write('Hello World!');
-    res.end();
+    for (const handler of handlers) {
+        if (!handler(req, res)) {
+            break;
+        }
+    }
 }).listen(port);

2b98a99c2f5c5255687ae34364c42aebacdbc9fa
atanasStoyanov <atanass@dxc.com>
2020-05-23 14:51:12 +0300

    http server setup

---
+++ JS Web/Intro to NodeJS/Cat Shelter Workshop/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = 3000;
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.write('Hello World!');
+    res.end();
+}).listen(port);

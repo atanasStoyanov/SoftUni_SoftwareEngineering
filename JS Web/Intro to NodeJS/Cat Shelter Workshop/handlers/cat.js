3bb0be0df06b31eac569218f45c502c3a3860d38
atanasStoyanov <atanass@dxc.com>
2020-05-25 23:20:32 +0300

    Add breed functionality done

--- JS Web/Intro to NodeJS/Cat Shelter Workshop/handlers/cat.js
+++ JS Web/Intro to NodeJS/Cat Shelter Workshop/handlers/cat.js
@@ -46,6 +46,42 @@ module.exports = (req, res) => {
             res.write(data);
             res.end();
         });
+    } else if (pathname === '/cats/addBreed' && req.method === 'POST') {
+        let formData = '';
+
+        req.on('data', data => {
+            formData += data;
+        });
+
+        req.on('end', () => {
+            const filePath = path.normalize(
+                path.join(__dirname, '../data/breeds.json'));
+
+            const body = qs.parse(formData);
+
+            fs.readFile(filePath, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.write('Page Not Found');
+                    return;
+                }
+
+                let breeds = JSON.parse(data);
+                breeds.push(body.breed);
+
+                fs.writeFile(filePath, JSON.stringify(breeds), err => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log('The breed was successfully added');
+                });
+
+                res.writeHead(301, { 'Location': '/' });
+                return res.end();
+            });
+        });
     } else {
         return true;
     }

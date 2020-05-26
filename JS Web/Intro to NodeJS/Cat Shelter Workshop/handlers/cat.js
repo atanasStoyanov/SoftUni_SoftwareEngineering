3f40b5e4a1efcbbc615a45d2a6913b52a6e74ebb
atanasStoyanov <atanass@dxc.com>
2020-05-26 22:44:21 +0300

    Add cat func

--- JS Web/Intro to NodeJS/Cat Shelter Workshop/handlers/cat.js
+++ JS Web/Intro to NodeJS/Cat Shelter Workshop/handlers/cat.js
@@ -2,7 +2,7 @@ const url = require('url');
 const fs = require('fs');
 const path = require('path');
 const qs = require('querystring');
-// const formidable = require('formidable');
+const formidable = require('formidable');
 const breeds = require('../data/breeds');
 const cats = require('../data/cats');
 
@@ -24,10 +24,78 @@ module.exports = (req, res) => {
                 return;
             }
 
+            const breedOptions = breeds.map(breed => `<option value="${breed}">${breed}</option>`);
+            const modifiedData = data.toString().replace('{{catBreeds}}', breedOptions);
+
             res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
+            res.write(modifiedData);
             res.end();
-        })
+        });
+
+    } else if (pathname === '/cats/addCat' && req.method === 'POST') {
+        let form = new formidable.IncomingForm();
+
+        form.parse(req, (err, fields, files) => {
+
+            if (err) {
+                console.log(err);
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.write('Page Not Found');
+                return;
+            }
+
+            const oldPath = files.upload.path;
+            const newPath = path.normalize(
+                path.join(__dirname, '../content/images/' + files.upload.name));
+
+
+            fs.rename(oldPath, newPath, err => {
+                if (err) {
+                    console.log(err);
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.write('Page Not Found');
+                    return;
+                }
+
+                console.log('File was uploaded successfully');
+            });
+
+            const filePath = path.normalize(
+                path.join(__dirname, '../data/cats.json'));
+
+            fs.readFile(filePath, 'utf-8', (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.write('Page Not Found');
+                    res.end();
+                    return;
+                }
+
+                let allCats = JSON.parse(data);
+                allCats.push({
+                    id: allCats.length + 1,
+                    ...fields,
+                    image: files.upload.name
+                });
+
+                fs.writeFile(filePath, JSON.stringify(allCats), err => {
+                    if (err) {
+                        console.log(err);
+                        res.writeHead(404, { 'Content-Type': 'text/plain' });
+                        res.write('Page Not Found');
+                        res.end();
+                        return;
+                    }
+
+                    console.log('Cat added successfully');
+                });
+
+                res.writeHead(301, { 'Location': '/' });
+                return res.end();
+            });
+        });
+
     } else if (pathname === '/cats/addBreed' && req.method === 'GET') {
 
         const filePath = path.normalize(
@@ -46,6 +114,7 @@ module.exports = (req, res) => {
             res.write(data);
             res.end();
         });
+
     } else if (pathname === '/cats/addBreed' && req.method === 'POST') {
         let formData = '';
 
